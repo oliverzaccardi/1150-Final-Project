899b39c2d115de7ba4ff068f39f25bc84869fe6b
oliverzaccardi <mc5641py@go.minnstate.edu>
2020-05-04 22:43:29 -0500

    Stopping point 5/4. Extracted data from table.

--- ITEC Courses.py
+++ ITEC Courses.py
@@ -1,38 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-get_HTML = requests.get(
+url = (
     'https://eservices.minnstate.edu/registration/search/advancedSubmit.html?campusid=305&searchrcid'
     '=0305&searchcampusid=305&yrtr=20205&subject=ITEC&courseNumber=&courseId=&openValue=OPEN_PLUS_WA'
     'ITLIST&delivery=ALL&showAdvanced=&starttime=&endtime=&mntransfer=&credittype=ALL&credits=&instr'
     'uctor=&keyword=&begindate=&site=&resultNumber=250')
 
-parse_HTML = BeautifulSoup(get_HTML.text, 'html.parser')
-data = parse_HTML.find_all('table')
-results = data[4]
-table_data = results.find_all('td')
+response = requests.get(url)
 
-records = []
-for result in results:
-    id_number = table_data[2].contents[0]
+soup = BeautifulSoup(response.content, 'html.parser')
+course_table = soup.find_all('table', id='resultsTable')
+course_table = course_table[0]
 
-    course_number = table_data[4].contents[0]
+results = []
+for row in course_table.find_all('tr'):
+    for cell in row.find_all('td'):
+        results.append(cell.text)
+print(results)
 
-    section_number = table_data[5].contents[0]
-
-    course = results.select('div>a')[3]
-    course_name = course.contents[0]
-
-    day = results.find('abbr')
-    course_day = day.contents[0]
-
-    time = table_data[9].text
-
-    course_credits = table_data[10].text
-
-    instructor = table_data[12].text
-    records.append((id_number, course_number, section_number, course_name, course_day, time, course_credits,
-                    instructor))
-print(records)
-# TODO
-#  extract spreadsheet column titles and data
+# TODO: figure out how to get rid of extra characters and add data to spreadsheet.
