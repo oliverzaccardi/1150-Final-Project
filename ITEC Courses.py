b6423790f490b156c65359a3eaa4ed1337bb8715
oliverzaccardi <mc5641py@go.minnstate.edu>
2020-05-07 12:33:18 -0500

    Stopping point 5/4. Extracted data from table.

--- ITEC Courses.py
+++ ITEC Courses.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from openpyxl import Workbook
 
 url = (
     'https://eservices.minnstate.edu/registration/search/advancedSubmit.html?campusid=305&searchrcid'
@@ -13,10 +14,29 @@ soup = BeautifulSoup(response.content, 'html.parser')
 course_table = soup.find_all('table', id='resultsTable')
 course_table = course_table[0]
 
-results = []
+workbook = Workbook()
+worksheet = workbook.active
+worksheet.title = 'ITEC Courses'
+
+worksheet.cell(1, 1, 'ID #')
+worksheet.cell(1, 2, 'Course Number - Section Number')
+worksheet.cell(1, 3, 'Course Title')
+worksheet.cell(1, 4, 'Day Class Meets')
+worksheet.cell(1, 5, 'Time Course Meets')
+worksheet.cell(1, 6, 'Credits')
+worksheet.cell(1, 7, 'Instructor')
+
+data = []
 for row in course_table.find_all('tr'):
     for cell in row.find_all('td'):
-        results.append(cell.text)
-print(results)
+        data.append(cell.text)
+
+for index, result in enumerate(data):
+    worksheet.cell(index + 1, 1, result)
+
+print(data)
+
+workbook.save('itec_courses.xlsx')
+
 
 # TODO: figure out how to get rid of extra characters and add data to spreadsheet.
